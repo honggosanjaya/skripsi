a984c8dc6d332e4c0e031bed4003aab9f8276c9c
RichieTjia17 <61897438+RichieTjia17@users.noreply.github.com>
2022-05-15 18:18:34 +0700

    Menambah Tabel SQL

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
+            $table->bigInteger('id_users');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('tabel',['staffs', 'customers']);
             $table->rememberToken();
             $table->timestamps();
         });
