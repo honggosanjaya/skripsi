f479658523f68578218338ade916ac7a56a80b0f
RichieTjia17 <61897438+RichieTjia17@users.noreply.github.com>
2022-05-16 20:25:31 +0700

    Tambah Dashboard Admin,Supervisor,Owner,Customer

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/main')
+
+@section('main_content')
+    <h3>Selamat datang, admin</h3>
+
+    <h5>Silakan pilih menu disamping</h5>
+@endsection
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -18,106 +18,106 @@
       </li>
     </ul>
 
-    @can('admin')
+    {{-- @can('admin') --}}
       <hr class="my-4" />
       <h1 class="mb-3 fs-6">Admin</h1>
       <ul class="p-0">
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-truck me-2"></i><span>Data Customer</span>
+            <i class="bi bi-people-fill me-2"></i><span>Data Customer</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="{{ Request::is('dashboard/pesanan*') ? 'active' : '' }}" href="/dashboard/pesanan">
-            <i class="bi bi-bag-check-fill me-2"></i><span>Pesanan</span>
+            <i class="bi bi-card-list me-2"></i><span>Pesanan</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="{{ Request::is('dashboard/retur*') ? 'active' : '' }}" href="/dashboard/retur">
-            <i class="bi bi-calendar3-event me-2"></i><span>Retur</span>
+            <i class="bi bi-arrow-return-left me-2"></i><span>Retur</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-bag-check-fill me-2"></i><span>Penjualan</span>
+            <i class="bi bi-cash me-2"></i><span>Penjualan</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="{{ Request::is('dashboard/produk*') ? 'active' : '' }}" href="/dashboard/produk/stok">
-            <i class="bi bi-bag-check-fill me-2"></i><span>Stok</span>
+            <i class="bi bi-box me-2"></i><span>Stok</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-bag-check-fill me-2"></i><span>Kendaraan</span>
+            <i class="bi bi-truck me-2"></i><span>Kendaraan</span>
           </a>
         </li>
       </ul>
-    @endcan
-
-    @can('supervisor')
+    {{-- @endcan --}}
+    
+    {{-- @can('supervisor') --}}
       <hr class="my-4" />
       <h1 class="mb-3 fs-6">Supervisor</h1>
       <ul class="p-0">
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Event</span>
+            <i class="bi bi-calendar3-event me-2"></i><span id="testing">Event</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="{{ Request::is('dashboard/pengguna*') ? 'active' : '' }}" href="/dashboard/pengguna">
-            <i class="bi bi-calendar3-event me-2"></i><span>Data Staf</span>
+            <i class="bi bi-people-fill me-2"></i><span>Data Staf</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-bag-check-fill me-2"></i><span>Penjualan</span>
+            <i class="bi bi-cash me-2"></i><span>Penjualan</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Kinerja Salesman</span>
+            <i class="bi bi-graph-up me-2"></i><span>Kinerja Salesman</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Limit Pembelian</span>
+            <i class="bi bi-wallet2 me-2"></i><span>Limit Pembelian</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Wilayah</span>
+            <i class="bi bi-geo-alt me-2"></i><span>Wilayah</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Jenis Customer</span>
+            <i class="bi bi-person-check me-2"></i><span>Jenis Customer</span>
           </a>
         </li>
       </ul>
-    @endcan
+    {{-- @endcan --}}
 
-    @can('Owner')
+    {{-- @can('Owner') --}}
       <hr class="my-4" />
       <h1 class="mb-3 fs-6">Owner</h1>
       <ul class="p-0">
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Penjualan</span>
+            <i class="bi bi-cash me-2"></i><span>Penjualan</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Kinerja Salesman</span>
+            <i class="bi bi-graph-up me-2"></i><span>Kinerja Salesman</span>
           </a>
         </li>
         <li class="mb-3">
           <a class="" href="">
-            <i class="bi bi-calendar3-event me-2"></i><span>Data Supervisor</span>
+            <i class="bi bi-people-fill me-2"></i><span>Data Supervisor</span>
           </a>
         </li>
       </ul>
-    @endcan
+    {{-- @endcan --}}
 
   </div>
 </div>
