f492b822225b026a70f4682b35957d398de4e5fa
Brichrist <hanjayachristian@gmail.com>
2022-05-22 16:24:39 +0700

    validasi email

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -68,6 +68,12 @@ class CustomerController extends Controller
         'telepon' => ['string', 'max:15'],
         'durasi_kunjungan' => ['required', 'integer'],
       ];
+      if ($request->id==null){
+        $rules['email'] = ['string', 'email', 'max:255', 'unique:users'];
+      }
+      if (Customer::find($request->id)->email == null) {
+        $rules['email'] = ['string', 'email', 'max:255', 'unique:users'];
+      }
       
       $validator = Validator::make($request->all(), $rules);
       $data = $request->except(['jam_masuk','alasan_penolakan','status','koordinat'])+['id_staff' => session('id_staff'), 'status' => 3,'created_at' => now()];
