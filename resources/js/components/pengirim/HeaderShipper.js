cc905fbe4a938fafa98229392167f2ac07158719
honggosanjaya <honggosanjaya@gmail.com>
2022-05-30 09:56:56 +0700

    melihat profil untuk shipper dan sales

--- resources/js/components/pengirim/HeaderShipper.js
+++ resources/js/components/pengirim/HeaderShipper.js
@@ -35,6 +35,11 @@ const HeaderShipper = ({ title, isDashboard }) => {
         console.log('eror logout', error.message);
       });
   }
+
+  const handleViewProfile = () => {
+    history.push('/salesman/profil');
+  }
+
   return (
     <header className='header_mobile d-flex justify-content-between align-items-center'>
       {!isDashboard &&
@@ -57,7 +62,14 @@ const HeaderShipper = ({ title, isDashboard }) => {
 
             <Dropdown.Menu>
               <Dropdown.Item>
-                <div className="btn btn-danger" onClick={handleLogout}>Logout</div>
+                <div className="btn btn-warning w-100" onClick={handleViewProfile}>
+                  <span className="iconify me-2" data-icon="carbon:user-profile"></span>Profil
+                </div>
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <div className="btn btn-danger w-100" onClick={handleLogout}>
+                  <span className="iconify me-2" data-icon="carbon:logout"></span>Logout
+                </div>
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
