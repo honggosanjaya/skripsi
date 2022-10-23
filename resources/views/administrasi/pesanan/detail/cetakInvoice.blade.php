2e6c27b1b3dba96ff86c76cd3a9e719d1c88b450
honggosanjaya <honggosanjaya@gmail.com>
2022-10-23 18:34:44 +0700

    penyesuaian invoice

--- resources/views/administrasi/pesanan/detail/cetakInvoice.blade.php
+++ resources/views/administrasi/pesanan/detail/cetakInvoice.blade.php
@@ -13,6 +13,7 @@
     th {
       border: 1px solid #000;
       width: calc(100% / 3);
+      padding: 0 5px;
     }
 
     .logo {
@@ -123,9 +124,9 @@
           <div class="logo">
             <img src="{{ public_path('images/icon-perusahaan.png') }}" width="70" height="70">
           </div>
-          <h5>{{ config('app.company_name') }}</h5>
-          <p>{{ config('app.company_address') }}</p>
-          <p>{{ config('app.company_contact') }}</p>
+          <h5>{{ config('app.company_name') ?? null }}</h5>
+          <p>{{ config('app.company_address') ?? null }}</p>
+          <p>{{ config('app.company_contact') ?? null }}</p>
         </div>
       </td>
       <td colspan="2" rowspan="2" class="td-medium">
@@ -138,7 +139,7 @@
 
     <tr>
       <td class="td-large">
-        <h6 class="uppercase-text margin0">&nbsp;{{ $order->linkCustomer->nama ?? null }}</h6>
+        <h6 class="uppercase-text margin0">{{ $order->linkCustomer->nama ?? null }}</h6>
       </td>
     </tr>
 
@@ -150,18 +151,26 @@
         <p class="margin0 center-text">Tgl. Faktur</p>
       </td>
       <td rowspan="4" class="td-large v-align-top">
-        <p class="margin0">&nbsp;{{ $order->linkCustomer->full_alamat ?? null }}</p>
-        <p class="margin0">&nbsp;{{ $order->linkCustomer->linkDistrict->nama }}</p>
-        <p class="margin0">&nbsp;{{ $order->linkCustomer->telepon }}</p>
+        <p class="margin0">{{ $order->linkCustomer->full_alamat ?? null }}</p>
+        <p class="margin0">{{ $order->linkCustomer->linkDistrict->nama ?? null }}</p>
+        <p class="margin0">{{ $order->linkCustomer->telepon ?? null }}</p>
       </td>
     </tr>
 
     <tr>
       <td class="td-medium">
-        <p class="margin0 center-text">{{ date('d/m/Y', strtotime($order->created_at ?? '-')) }}</p>
+        @if ($order->linkOrderTrack->waktu_order ?? null)
+          <p class="margin0 center-text">{{ date('d M Y', strtotime($order->linkOrderTrack->waktu_order)) }}
+          </p>
+        @else
+          <p></p>
+        @endif
       </td>
       <td class="td-medium">
-        <p class="margin0 center-text">{{ date('d M Y', strtotime($order->linkInvoice->updated_at ?? '-')) }}</p>
+        @if ($order->linkOrderTrack->waktu_berangkat ?? null)
+          <p class="margin0 center-text">{{ date('d M Y', strtotime($order->linkOrderTrack->waktu_berangkat)) }}
+          </p>
+        @endif
       </td>
     </tr>
 
@@ -173,7 +182,7 @@
         <p class="margin0 center-text">Salesman</p>
       </td>
       <td class="td-small">
-        <p class="margin0 center-text">Kredit/TOP</p>
+        <p class="margin0 center-text">TOP</p>
       </td>
       <td class="td-medium">
         <p class="margin0 center-text">Tgl. JATUH TEMPO</p>
@@ -191,10 +200,14 @@
         <p class="margin0 center-text">{{ $order->linkStaff->nama ?? null }}</p>
       </td>
       <td class="td-small">
-        <p class="margin0 center-text">NET 14</p>
+        <p class="margin0 center-text">NET {{ $order->linkInvoice->jatuh_tempo ?? null }}</p>
       </td>
       <td class="td-medium">
-        <p class="margin0 center-text">{{ $order->linkInvoice->jatuh_tempo ?? null }}</p>
+        @if (($order->linkOrderTrack->waktu_berangkat ?? null) && ($order->linkInvoice->jatuh_tempo ?? null))
+          <p class="margin0 center-text">
+            {{ date('d M Y', strtotime($order->linkOrderTrack->waktu_berangkat . ' + ' . $order->linkInvoice->jatuh_tempo . ' days')) }}
+          </p>
+        @endif
       </td>
       <td class="td-medium">
         <p class="margin0 center-text">{{ $order->linkInvoice->nomor_invoice ?? null }}</p>
@@ -223,19 +236,34 @@
         <td class="uppercase-text">{{ $item->linkItem->nama ?? null }}</td>
         <td class="center-text">{{ $item->kuantitas ?? null }}</td>
         <td class="uppercase-text center-text">{{ $item->linkItem->satuan ?? null }}</td>
-        <td align="right">{{ number_format($item->harga_satuan ?? 0, 0, '', '.') }}&nbsp;</td>
-        <td class="center-text">20</td>
-        <td align="right">{{ number_format($item->harga_satuan ?? 0, 0, '', '.') }}&nbsp;</td>
-        <td align="right">{{ number_format(($item->harga_satuan ?? 0) * ($item->kuantitas ?? 0), 0, '', '.') }}&nbsp;
+
+        @if (($order->linkCustomer->linkCustomerType->diskon ?? null) && ($item->harga_satuan ?? null))
+          <td align="right">
+            {{ number_format(($item->harga_satuan / (100 - $order->linkCustomer->linkCustomerType->diskon)) * 100, 0, '', '.') }}
+          </td>
+          <td class="center-text">{{ $order->linkCustomer->linkCustomerType->diskon }}</td>
+        @else
+          <td></td>
+          <td></td>
+        @endif
+
+        <td align="right">{{ number_format($item->harga_satuan ?? 0, 0, '', '.') }}</td>
+        <td align="right">{{ number_format(($item->harga_satuan ?? 0) * ($item->kuantitas ?? 0), 0, '', '.') }}
         </td>
       </tr>
     @endforeach
 
     @php
-      $subtotal = 0;
-      foreach ($orderitems as $item) {
-          $subtotal = $subtotal + ($item->kuantitas ?? 0) * ($item->harga_satuan ?? 0);
+      $diskon = $order->linkInvoice->linkEvent->diskon ?? null;
+      $potongan = $order->linkInvoice->linkEvent->potongan ?? null;
+      
+      if ($diskon ?? null) {
+          $totaldiskon = (($order->linkInvoice->harga_total ?? 0) / (100 - $diskon)) * $diskon;
+      } else {
+          $totaldiskon = $potongan;
       }
+      
+      $subtotal = ($order->linkInvoice->harga_total ?? 0) + ($totaldiskon ?? 0);
     @endphp
 
     <tr class="order-item-big">
@@ -261,30 +289,25 @@
         <p class="margin0 center-text border-top">Tgl:</p>
       </td>
       <td colspan="2" align="right">
-        <p class="margin0 font-bold">Total Sub :&nbsp;</p>
+        <p class="margin0 font-bold">Total Sub :</p>
       </td>
-      <td align="right" class="font-bold">{{ number_format($subtotal ?? 0, 0, '', '.') }}&nbsp;</td>
+      <td align="right" class="font-bold">{{ number_format($subtotal, 0, '', '.') }}</td>
     </tr>
-
-    @php
-      $diskon = $order->linkInvoice->linkEvent->diskon ?? null;
-      $potongan = $order->linkInvoice->linkEvent->potongan ?? null;
-    @endphp
 
     <tr>
       <td colspan="2" align="right">
-        <p class="margin0 font-bold">Diskon % :&nbsp;</p>
+        <p class="margin0 font-bold">Diskon {{ $potongan != null ? '' : '%' }} :</p>
       </td>
       <td align="right" class="font-bold">
-        {{ $diskon == null ? number_format($potongan ?? 0, 0, '', '.') : $diskon }}&nbsp;
+        {{ number_format($totaldiskon ?? 0, 0, '', '.') }}
       </td>
     </tr>
 
     <tr>
       <td colspan="2" align="right">
-        <p class="margin0 font-bold">Total Faktur :&nbsp;</p>
+        <p class="margin0 font-bold">Total Faktur :</p>
       </td>
-      <td align="right" class="font-bold">{{ number_format($order->linkInvoice->harga_total ?? 0, 0, '', '.') }}&nbsp;
+      <td align="right" class="font-bold">{{ number_format($order->linkInvoice->harga_total ?? 0, 0, '', '.') }}
       </td>
     </tr>
   </table>
